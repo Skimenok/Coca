8a660e2cb2e30f8ef3dcb3984bf60faa0fb5088f
Skimenok <iulybin44@gmail.com>
2025-09-10 21:42:05 +0300

    started home

--- src/js/components/theme.js
+++ src/js/components/theme.js
@@ -1,23 +1,28 @@
 export const useTheme = () => {
   let themeBody = document.querySelector("[data-theme]"),
     checkbox = document.querySelector("[data-theme-switcher]"),
-    themeTarget = themeBody.dataset;
-  const burgerMenu = document.querySelector('[data-burger="menu"]');
+    themeTarget = themeBody.dataset,
+    themeButton = document.querySelector(
+      ".header__switcher-text",
+    );
 
   let theme = localStorage.getItem("theme") || "light";
   themeTarget.theme = theme;
 
   if (themeTarget.theme == "dark") {
     checkbox.checked = true;
+    themeButton.textContent = "Light";
   }
 
   checkbox.addEventListener("click", () => {
     if (themeTarget.theme == "light") {
       themeTarget.theme = "dark";
       localStorage.setItem("theme", "dark");
+      themeButton.textContent = "Light";
     } else {
       themeTarget.theme = "light";
       localStorage.setItem("theme", "light");
+      themeButton.textContent = "Dark";
     }
   });
 };
